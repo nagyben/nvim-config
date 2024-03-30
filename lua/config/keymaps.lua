3c8939b0f3f06f990585d29cdc96b46123f8a3f9
Ben Nagy <nagy.benb@gmail.com>
2024-03-30 18:43:25 +0000

    feat: initial config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "v" }, "J", "'5j'", { expr = true, silent = true, remap = false })
+vim.keymap.set({ "n", "v" }, "K", "'5k'", { expr = true, silent = true, remap = false })
+vim.keymap.set({ "n" }, "gh", vim.lsp.buf.hover, { silent = true, noremap = true })
