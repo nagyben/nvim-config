cf0f87fd2a9dd8e7fb91f8bc5d1911e23ec26444
Ben Nagy <nagy.benb@gmail.com>
2024-03-31 18:54:31 +0100

    fix: modify telescope keybinds

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -2,9 +2,16 @@ return {
   "nvim-telescope/telescope-file-browser.nvim",
   keys = {
     {
-      "<leader>sB",
+      "<leader><space>",
       ":Telescope file_browser path=%:p:h=%:p:h<cr>",
       desc = "Browse files",
+    },
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
     },
   },
   config = function()
