f44b9cefe938d4af6775e33f757165b9103d2739
Ben Nagy <nagy.benb@gmail.com>
2024-04-02 08:32:47 +0100

    fix: auto save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Configure autosave (see :h events for BufLeave and FocusLost)
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  command = "silent! wa",
+})
