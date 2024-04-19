f9a2368aaad66503415111267ccf15f886c74fb0
Ben Nagy <ben.b.nagy@gsk.com>
2024-04-19 09:45:32 +0100

    fix: fast typing shift+ terminal escape issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,7 @@ vim.keymap.set({ "n", "v" }, "J", "'5j'", { expr = true, silent = true, remap = 
 vim.keymap.set({ "n", "v" }, "K", "'5k'", { expr = true, silent = true, remap = false })
 vim.keymap.set({ "n" }, "gh", vim.lsp.buf.hover, { silent = true, noremap = true })
 vim.keymap.set({ "n" }, "<leader>fG", "<cmd>Telescope live_grep<CR>", { desc = "Live grep file contents" })
+vim.keymap.set({ "n" }, "<leader><space>", "<cmd>Telescope file_browser<CR>", { desc = "Live file browser" })
 
 local lazyterm = function()
   LazyVim.terminal(nil, { cwd = LazyVim.root() })
@@ -13,3 +14,7 @@ end
 vim.keymap.set({ "!", "", "l" }, "<c-`>", lazyterm, { desc = "Terminal (Root Dir)" })
 vim.keymap.set("t", "<c-`>", "<cmd>close<cr>", { desc = "Close Terminal" })
 vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>", { desc = "Open URL under cursor" })
+
+-- prevent shift key escape characters in terminal mode
+vim.keymap.set("t", "<s-space>", "<space>", {})
+vim.keymap.set("t", "<s-enter>", "<enter>", {})
