7a391294fc766ab092123c3bd8acd5ddae883a17
Ben Nagy <ben.b.nagy@gsk.com>
2024-05-08 21:05:04 +0100

    fix: search hidden files with live grep

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  config = {
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+    },
+  },
+}
