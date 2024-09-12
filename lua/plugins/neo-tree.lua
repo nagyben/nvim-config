8997e0e3c3547ea63bc41efd3d1fd7dbcb7f0d1c
Ben Nagy <ben.b.nagy@gsk.com>
2024-09-12 08:52:17 +0100

    fix: update neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -21,15 +21,21 @@ return {
         never_show = {},
       },
       bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
-      cwd_target = {
-        sidebar = "tab", -- sidebar is when position = left or right
-        current = "window", -- current is when position = current
-      },
+      -- follow_current_file = { enabled = true },
+      -- cwd_target = {
+      --   sidebar = "tab", -- sidebar is when position = left or right
+      --   current = "window", -- current is when position = current
+      --   -- sidebar = "global",
+      --   -- current = "global",
+      -- },
     },
     event_handlers = {
       {
-        event = "file_opened",
-        handler = function(file_path)
+        event = "file_open_requested",
+        handler = function()
+          -- auto close
+          -- vim.cmd("Neotree close")
+          -- OR
           require("neo-tree.command").execute({ action = "close" })
         end,
       },
