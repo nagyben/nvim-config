8f2521447a5de3e6ec82425415a340d89c35d647
Ben Nagy <nagy.benb@gmail.com>
2024-10-22 21:41:56 +0100

    feat: transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,22 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      color_overrides = {
+        latte = {
+          base = "#ff0000",
+          mantle = "#242424",
+          crust = "#474747",
+        },
+        frappe = {},
+        macchiato = {},
+        mocha = {},
+      },
+    },
+  },
   {
     "rockyzhang24/arctic.nvim",
     branch = "v2",
@@ -11,4 +28,5 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  { "rebelot/kanagawa.nvim" },
 }
