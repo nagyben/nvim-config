21735e6e14ce3a15fb7f3b5f22ab54786d6659f3
Ben Nagy <ben.b.nagy@gsk.com>
2025-03-31 11:06:31 +0100

    fix: update venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,15 +1,13 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-  opts = {
-    -- Your options go here
-    -- name = "venv",
-    -- auto_refresh = false
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
+  lazy = false,
   branch = "regexp", -- This is the regexp branch, use this for the new version
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
-    { "<leader>cV", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" },
-    { "<leader>cv", "<cmd>:VenvSelectCached<cr>", desc = "Select VirtualEnv (cached)" },
+    { "cv", "<cmd>VenvSelect<cr>" },
+    { "cV", "<cmd>VenvSelect<cr>" },
   },
 }
