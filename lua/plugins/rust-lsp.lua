ccd9d7a603cd1353288fff67142016749783017c
Ben Nagy <ben.b.nagy@gsk.com>
2025-05-15 10:13:59 +0100

    fix: remove annoying rust diagnostics

--- lua/plugins/rust-lsp.lua
+++ lua/plugins/rust-lsp.lua
@@ -1,37 +1,14 @@
 return {
-  "neovim/nvim-lspconfig",
+  "mrcjkb/rustaceanvim",
   opts = {
-    servers = {
-      rust_analyzer = {},
-      taplo = {
-        keys = {
-          {
-            "gh",
-            function()
-              if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-                require("crates").show_popup()
-              else
-                vim.lsp.buf.hover()
-              end
-            end,
-            desc = "Show Crate Documentation",
+    server = {
+      default_settings = {
+        ["rust-analyzer"] = {
+          diagnostics = {
+            disabled = { "proc-macro-disabled" },
           },
         },
       },
-    },
-    setup = {
-      rust_analyzer = function()
-        for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-          local default_diagnostic_handler = vim.lsp.handlers[method]
-          vim.lsp.handlers[method] = function(err, result, context, config)
-            if err ~= nil and err.code == -32802 then
-              return
-            end
-            return default_diagnostic_handler(err, result, context, config)
-          end
-        end
-        return true
-      end,
     },
   },
 }
