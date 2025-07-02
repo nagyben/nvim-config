bc45fb34b20c642272872bb6b0f6281d85edbf56
Ben Nagy <ben.b.nagy@gsk.com>
2025-07-02 10:14:31 +0100

    chore: disable supertab

--- lua/plugins/tab-completion.lua
+++ lua/plugins/tab-completion.lua
@@ -6,7 +6,9 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = "super-tab" },
+    --
+    -- leave commented until https://github.com/LazyVim/LazyVim/pull/6183 is merged
+    -- keymap = { preset = "super-tab" },
     cmdline = { sources = { "cmdline" } },
     sources = {
       default = { "lsp", "path", "buffer", "codecompanion" },
