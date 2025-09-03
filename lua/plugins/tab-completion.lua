2897a1baa592f8dfb8bb974239c6798687c62c4f
Ben Nagy <ben.b.nagy@gsk.com>
2025-09-03 17:58:00 +0100

    fix: super-tab

--- lua/plugins/tab-completion.lua
+++ lua/plugins/tab-completion.lua
@@ -1,17 +1,31 @@
+-- return {
+--   "saghen/blink.cmp",
+--   opts = {
+--     -- 'default' for mappings similar to built-in completion
+--     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+--     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+--     -- see the "default configuration" section below for full documentation on how to define
+--     -- your own keymap.
+--     --
+--     -- leave commented until https://github.com/LazyVim/LazyVim/pull/6183 is merged
+--     -- keymap = { preset = "super-tab" },
+--     cmdline = { sources = { "cmdline" } },
+--     sources = {
+--       default = { "lsp", "path", "buffer", "codecompanion" },
+--     },
+--   },
+-- }
+
 return {
   "saghen/blink.cmp",
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
-    --
-    -- leave commented until https://github.com/LazyVim/LazyVim/pull/6183 is merged
-    -- keymap = { preset = "super-tab" },
-    cmdline = { sources = { "cmdline" } },
-    sources = {
-      default = { "lsp", "path", "buffer", "codecompanion" },
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
     },
   },
 }
