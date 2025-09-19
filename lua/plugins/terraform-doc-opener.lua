16486292b8123875e6bb48d0a2b3c3adcbb71d5b
Ben Nagy <nagy.benb@gmail.com>
2025-09-19 17:11:37 +0100

    fix: disable terraform-doc-opener

--- lua/plugins/terraform-doc-opener.lua
+++ lua/plugins/terraform-doc-opener.lua
@@ -1,14 +1,18 @@
-return {
-  { "Afourcat/treesitter-terraform-doc.nvim" },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        terraformls = function()
-          require("treesitter-terraform-doc").setup({})
-        end,
+if true then
+  return {}
+else
+  return {
+    { "Afourcat/treesitter-terraform-doc.nvim" },
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+        setup = {
+          terraformls = function()
+            require("treesitter-terraform-doc").setup({})
+          end,
+        },
       },
     },
-  },
-}
+  }
+end
