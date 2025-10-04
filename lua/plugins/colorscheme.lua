a10e24f81316d5117b2cc2a775407c62b2324dcd
Ben Nagy <ben.b.nagy@gsk.com>
2025-10-04 14:20:17 +0100

    fix(colorscheme): revert to default catppuccin settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,19 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      transparent_background = true,
-      color_overrides = {
-        latte = {
-          base = "#ff0000",
-          mantle = "#242424",
-          crust = "#474747",
-        },
-        frappe = {},
-        macchiato = {},
-        mocha = {},
-      },
-    },
   },
   {
     "LazyVim/LazyVim",
