000bdc342abe53be83f1b150fde7876eec9ade9f
Ben Nagy <ben.b.nagy@gsk.com>
2026-01-01 16:02:41 +0000

    fix: use ty for python LSP

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,4 +8,5 @@ vim.opt.scrolloff = 15
 -- vim.g.root_spec = { "cwd" } -- https://github.com/LazyVim/LazyVim/discussions/2150
 vim.g.root_spec = { ".git" }
 vim.g.snacks_animate = false
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({ virtual_lines = { current_line = true } })
+vim.g.lazyvim_python_lsp = "ty"
