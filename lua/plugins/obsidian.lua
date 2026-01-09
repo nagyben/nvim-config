d010ead9ebc1a6106d4168addb61bb90d21bce22
Ben Nagy <ben.b.nagy@gsk.com>
2026-01-09 10:37:41 +0000

    feat: add obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  -- ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre "
+  --     .. vim.fn.expand("~")
+  --     .. "/obsidian-vaults/nagyben/*.md",
+  --   "BufNewFile " .. vim.fn.expand("~") .. "/obsidian-vaults/nagyben/*.md",
+  -- },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/obsidian-vaults/nagyben",
+      },
+    },
+
+    -- see below for full list of options 👇
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+    daily_notes = {
+      folder = "dailies",
+      template = "dailies.md",
+    },
+  },
+}
