75ba8697c9838a6ded027fabb65ca96cd07ba5cd
Ben Nagy <nagy.benb@gmail.com>
2026-02-24 15:34:18 +0000

    feat(diffview): add keybinds

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,1 +1,25 @@
-return { "sindrets/diffview.nvim" }
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>dvo", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open" },
+    { "<leader>dvc", "<cmd>DiffviewClose<cr>", desc = "DiffView Close" },
+    { "<leader>dvh", "<cmd>DiffviewFileHistory<cr>", desc = "DiffView File History" },
+  },
+  config = function()
+    local actions = require("diffview.actions")
+    require("diffview").setup({
+      keymaps = {
+        disable_defaults = false,
+        view = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+        },
+        file_panel = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+        },
+        file_history_panel = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+        },
+      },
+    })
+  end,
+}
